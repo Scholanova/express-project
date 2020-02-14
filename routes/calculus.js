281fb1faa3c37515e9270bc9cb48f953337b2444
Antoine Boileau <antoine@boileau.io>
2020-02-14 13:48:55 +0100

    ADD Error on odd result and counter increment cookie

--- routes/calculus.js
+++ routes/calculus.js
@@ -7,16 +7,29 @@ router.get('/', function (req, res, next) {
 })
 
 router.post('/', function (req, res, next) {
+  if(req.cookies['calculus_counter']) {
+    res.cookie(
+      'calculus_counter',
+      parseInt(req.cookies['calculus_counter']) + 1
+    )
+  } else {
+    res.cookie('calculus_counter', 0)
+  }
+  next()
+})
+
+
+router.post('/', function (req, res, next) {
   calculusService.sum(req.body['First operand'], req.body['Second operand'])
     .then((result) => {
-      res.cookie('calculus-result', result)
+      res.cookie('calculus_result', result)
         .redirect('/calculus/result')
     })
     .catch(next)
 })
 
 router.get('/result', function (req, res, next) {
-  const result = req.cookies['calculus-result']
+  const result = req.cookies['calculus_result']
   res.render('calculus/result', { result })
 })
 
--- services/calculusService.js
+++ services/calculusService.js
@@ -1,8 +1,16 @@
+const { NotEvenResultError } = require('../errors')
+
 const calculusService = {
   sum: (operand1, operand2) => {
     return Promise.resolve([operand1, operand2])
       .then(([operand1, operand2]) => {
-        return operand1 + operand2
+        return parseInt(operand1) + parseInt(operand2)
+      })
+      .then((result) => {
+        if (result % 2 !== 0) {
+          throw new NotEvenResultError(result)
+        }
+        return result
       })
   }
 }

139c634a8a21d27058b1bf24fbda5b4b7d888c35
Antoine Boileau <antoine@boileau.io>
2020-02-14 09:59:43 +0100

    ADD Middleware to log query params on all /users routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,16 @@ var express = require('express')
 var router = express.Router()
 
 /* GET users listing. */
+
+router.use(function (req, res, next) {
+  const queryLogString = Object.entries(req.query)
+    .reduce(
+      (queryLogString, [key, value]) => queryLogString + ` ${key}=${value}`
+      , '')
+  console.log('> query params:', queryLogString)
+  next()
+})
+
 router.get('/', function (req, res, next) {
   const numberOfUser = Math.min(req.query.nb || 2, 10000)
 
